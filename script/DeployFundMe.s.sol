4db3479ad1621031b1bb4ee033adb5e0a0ce1eda
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-07-10 23:09:29 +0200

    Added integration tests code

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,13 +6,17 @@ import {FundMe} from "../src/FundMe.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
-  function run() external returns (FundMe){
-    HelperConfig helperConfig = new HelperConfig();
-    address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
-    
-    vm.startBroadcast();
-    FundMe fundMe = new FundMe(ethUsdPriceFeed);
-    vm.stopBroadcast();
-    return fundMe;
-  }
+  function deployFundMe() public returns (FundMe, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        address priceFeed = helperConfig.getConfigByChainId(block.chainid).priceFeed;
+
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(priceFeed);
+        vm.stopBroadcast();
+        return (fundMe, helperConfig);
+    }
+
+    function run() external returns (FundMe, HelperConfig) {
+        return deployFundMe();
+    }
 }
